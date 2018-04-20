8e218f25d044cb9517fba9ce93e268e97df28b28
Peter Luick <pluick@tux-35.cae.wisc.edu>
2018-04-20 12:11:46 -0500

    ram and rom

---
+++ ram.sv
@@ -0,0 +1,25 @@
+module ram (data, addr, we, clk, q);
+ parameter ADDR_WIDTH = 0;
+ parameter DATA_WIDTH = 0;
+ parameter FILE_IN = "ram_output_contents.txt";
+ input [(DATA_WIDTH-1):0] data;
+ input [(ADDR_WIDTH-1):0] addr;
+ input we, clk;
+ output [(DATA_WIDTH-1):0] q;
+ // Declare the RAM variable
+ reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+ // Variable to hold the registered read address
+ reg [ADDR_WIDTH-1:0] addr_reg;
+
+ initial begin
+ readmemh(FILE_IN, ram);
+ end
+
+ always @ (posedge clk)
+ begin
+ if (we) // Write
+ ram[addr] <= data;
+ addr_reg <= addr;
+ end
+ assign q = ram[addr_reg];
+endmodule
---
+++ rom.sv
@@ -0,0 +1,17 @@
+module rom (addr, clk, q);
+ parameter ADDR_WIDTH = 0;
+ parameter DATA_WIDTH = 0;
+ parameter FILE_IN = "ram_output_contents.txt";
+ input [(ADDR_WIDTH-1):0] addr;
+ input clk;
+ output reg [(DATA_WIDTH-1):0] q;
+ // Declare the ROM variable
+ reg [DATA_WIDTH-1:0] rom[2**ADDR_WIDTH-1:0];
+ initial begin
+ readmemh(FILE_IN, rom);
+ end
+ always @ (posedge clk)
+ begin
+ q <= rom[addr];
+ end
+endmodule 
