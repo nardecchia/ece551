094f2f9bbfcee2dedb4b815507de00d1cdd9dd32
nardecchia <nardecchia@wisc.edu>
2018-04-27 11:23:23 -0500

    duplicated RAM module for snn and instantiated in snn

--- ram.sv
+++ ram.sv
@@ -23,3 +23,30 @@ module ram (data, addr, we, clk, q);
  end
  assign q = ram[addr_reg];
 endmodule
+
+
+
+
+
+module ram_snn (data, addr, we, clk, q);
+ input [7:0] data;
+ input [9:0] addr;
+ input we, clk;
+ output q;
+ // Declare the RAM variable
+ reg [7:0] ram[2**9:0];
+ // Variable to hold the registered read address
+ reg [9:0] addr_reg;
+
+// initial begin
+	//$readmemh(FILE_IN, ram);
+ //end
+
+ always @ (posedge clk)
+ begin
+ if (we) // Write
+ ram[addr] <= data;
+ addr_reg <= addr;
+ end
+ assign q = ram[addr_reg];
+endmodule
